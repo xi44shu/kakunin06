b173486f00f554a22989e07b2cc8e20bac5f75ed
joni-deppu <44shuxichuan@gmail.com>
2022-09-21 19:05:31 +0900

    timezone作成

---
+++ app/models/time_zone.rb
@@ -0,0 +1,12 @@
+class TimeZone < ActiveHash::Base
+  self.data = [
+    { id: 1, name: '---' },
+    { id: 2, name: '午前' },
+    { id: 3, name: '午後' },
+    { id: 4, name: '夜間' },
+  ]
+
+  # include ActiveHash::Associations
+  # has_many :schedules
+
+end
