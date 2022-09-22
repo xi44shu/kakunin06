73968e9714dbbf277e21f5147e816713f0de61af
joni-deppu <44shuxichuan@gmail.com>
2022-09-22 18:02:13 +0900

    ログインの結合テスト

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -53,51 +53,47 @@ RSpec.describe "Users", type: :system do
   end
 end
 
-# RSpec.describe "ログイン", type: :system do
-#   before do
-#     @user = FactoryBot.create(:user)
-#   end
-#   context 'ログインができるとき' do 
-#     it '正しい情報を入力すればログインができてトップページに移動する' do
-#       # トップページに移動する
-#       visit root_path
-#       # トップページにログインページへ遷移するボタンがあることを確認する
-#       expect(page).to have_content('ログイン')
-#       # ログインページへ移動する
-#       visit new_user_registration_path
-#       # ユーザー情報を入力する
-#       fill_in '名前', with: @user.name
-#       fill_in 'パスワード', with: @user.password
-#       # ログインボタンを押す
-#         find('input[name="commit"]').click
-#       # トップページへ遷移したことを確認する
-#       expect(current_path).to eq(root_path)
-#       # トップページにログアウトページへ遷移するボタンがあることを確認する
-#       expect(page).to have_content('ログアウト')
-#       # サインアップページへ遷移するボタンや、ログインページへ遷移するボタンが表示されていないことを確認する      
-#       expect(page).to have_no_content('新規登録')
-#       expect(page).to have_no_content('ログイン')
-#     end
-#   end
-#   context 'ログインができないとき' do
-#     it '誤った情報ではログインができずに新規登録ページへ戻ってくる' do
-#       # トップページに移動する
-#       visit root_path
-#       # トップページにサインアップページへ遷移するボタンがあることを確認する
-#       expect(page).to have_content('新規登録')
-#       # 新規登録ページへ移動する
-#       visit new_user_registration_path
-#       # ユーザー情報を入力する
-#       fill_in '名前', with: ""
-#       fill_in 'Eメール', with: ""
-#       fill_in 'パスワード', with: ""
-#       fill_in 'パスワード（確認用）', with: ""
-#        # サインアップボタンを押してもユーザーモデルのカウントは上がらないことを確認する
-#       expect{
-#         find('input[name="commit"]').click
-#       }.to change { User.count }.by(0)
-#       # 新規登録ページへ戻されることを確認する
-#       expect(current_path).to eq user_registration_path
-#     end
-#   end
-# end
+RSpec.describe "ログイン", type: :system do
+  before do
+    @user = FactoryBot.create(:user)
+  end
+  context 'ログインができるとき' do 
+    it '正しい情報を入力すればログインができてトップページに移動する' do
+      # トップページに移動する
+      visit root_path
+      # トップページにログインページへ遷移するボタンがあることを確認する
+      expect(page).to have_content('ログイン')
+      # ログインページへ移動する
+      visit new_user_session_path
+      # ユーザー情報を入力する
+      fill_in '名前', with: @user.name
+      fill_in 'パスワード', with: @user.password
+      # ログインボタンを押す
+        find('input[name="commit"]').click
+      # トップページへ遷移したことを確認する
+      expect(current_path).to eq(root_path)
+      # トップページにログアウトページへ遷移するボタンがあることを確認する
+      expect(page).to have_content('ログアウト')
+      # サインアップページへ遷移するボタンや、ログインページへ遷移するボタンが表示されていないことを確認する      
+      expect(page).to have_no_content('新規登録')
+      expect(page).to have_no_content('ログイン')
+    end
+  end
+  context 'ログインができないとき' do
+    it '誤った情報ではログインができずに新規登録ページへ戻ってくる' do
+      # トップページに移動する
+      visit root_path
+      # トップページにログインページへ遷移するボタンがあることを確認する
+      expect(page).to have_content('ログイン')
+      # ログインページへ移動する
+      visit new_user_session_path
+      # ユーザー情報を入力する
+      fill_in '名前', with: ""
+      fill_in 'パスワード', with: ""
+      # ログインボタンを押す
+        find('input[name="commit"]').click
+      # ログインページへ戻されることを確認する
+      expect(current_path).to eq user_session_path
+    end
+  end
+end
