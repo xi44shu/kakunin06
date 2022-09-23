14544c9418b2a0d0452360736926237c601d9449
joni-deppu <44shuxichuan@gmail.com>
2022-09-23 13:08:16 +0900

    scheduleの結合テスト03
    削除機能

--- spec/system/schedules_spec.rb
+++ spec/system/schedules_spec.rb
@@ -46,6 +46,54 @@ require 'rails_helper'
 #   end
 # end
 
+# RSpec.describe "編集", type: :system do
+#   before do
+#     @user = FactoryBot.create(:user)
+#     @team = FactoryBot.build(:team)
+#     @team.work = 'true'
+#     @team.save
+#     @schedule = FactoryBot.create(:schedule)
+#   end
+
+#   context '予約の編集ができるとき'do
+#     it 'ログインしたユーザーは予約の編集ができる' do
+#       # ログインする
+#       visit new_user_session_path
+#       fill_in '名前', with: @user.name
+#       fill_in 'パスワード', with: @user.password
+#       find('input[name="commit"]').click
+#       expect(current_path).to eq(root_path)
+#       # 先ほど作成した予約が存在することを確認する
+#       visit schedule_path(@schedule.id)
+#       # 予約編集ページへのリンクがあることを確認する
+#       expect(page).to have_content('予約を変更')
+#       click_link '予約を変更'
+#       # 予約編集ページに遷移する
+#       expect(current_path).to eq(edit_schedule_path(@schedule.id))
+#       # フォームに情報を入力する
+#         fill_in 'schedule[scheduled_date]', with: (@schedule.scheduled_date)+1
+#         select "午後", from: 'schedule[time_zone_id]'
+#         select "中", from: 'schedule[size_id]'
+#         select "本予約", from: 'schedule[accuracy_id]'
+#         select "伊勢", from: 'schedule[mie_id]'
+#         select "商社", from: 'schedule[first_contact_id]'
+#         # click_button '予約を変更する'
+#         # 編集してもTweetモデルのカウントは変わらないことを確認する
+#         expect{
+#           find('input[name="commit"]').click
+#         }.to change { Schedule.count }.by(0)
+#     end
+#   end
+#   context '予約編集登録ができないとき'do
+#     it 'ログインしていないと予約編集ページに遷移できない' do
+#       # トップページに遷移する
+#       visit root_path
+#       # 新規投稿ページへのリンクがないことを確認する
+#       expect(page).to have_no_content('予約を変更')
+#     end
+#   end
+# end
+
 RSpec.describe "編集", type: :system do
   before do
     @user = FactoryBot.create(:user)
@@ -55,8 +103,8 @@ RSpec.describe "編集", type: :system do
     @schedule = FactoryBot.create(:schedule)
   end
 
-  context '予約の編集ができるとき'do
-    it 'ログインしたユーザーは予約の編集ができる' do
+  context '予約の削除ができるとき'do
+    it 'ログインしたユーザーは予約の削除ができる' do
       # ログインする
       visit new_user_session_path
       fill_in '名前', with: @user.name
@@ -65,27 +113,18 @@ RSpec.describe "編集", type: :system do
       expect(current_path).to eq(root_path)
       # 先ほど作成した予約が存在することを確認する
       visit schedule_path(@schedule.id)
-      # 予約編集ページへのリンクがあることを確認する
-      expect(page).to have_content('予約を変更')
-      click_link '予約を変更'
-      # 予約編集ページに遷移する
-      expect(current_path).to eq(edit_schedule_path(@schedule.id))
-      # フォームに情報を入力する
-        fill_in 'schedule[scheduled_date]', with: (@schedule.scheduled_date)+1
-        select "午後", from: 'schedule[time_zone_id]'
-        select "中", from: 'schedule[size_id]'
-        select "本予約", from: 'schedule[accuracy_id]'
-        select "伊勢", from: 'schedule[mie_id]'
-        select "商社", from: 'schedule[first_contact_id]'
-        # click_button '予約を変更する'
-        # 編集してもTweetモデルのカウントは変わらないことを確認する
-        expect{
-          find('input[name="commit"]').click
-        }.to change { Schedule.count }.by(0)
+      # 予約削除ページへのリンクがあることを確認する
+      expect(page).to have_content('予約を削除')
+      # 投稿を削除するとレコードの数が1減ることを確認する
+      expect{
+        find_link('予約を削除', href: schedule_path(@schedule.id)).click
+      }.to change { Schedule.count }.by(-1)
+      # トップページに遷移する
+      visit root_path
     end
   end
-  context '予約編集登録ができないとき'do
-    it 'ログインしていないと予約編集ページに遷移できない' do
+  context '予約削除登録ができないとき'do
+    it 'ログインしていないと予約削除ページに遷移できない' do
       # トップページに遷移する
       visit root_path
       # 新規投稿ページへのリンクがないことを確認する
